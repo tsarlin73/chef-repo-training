e7f5ce19984c1f490b4a1c34d051d7be8e334a39
ho.tsarlin <tsarlin73@myorganization.com>
2017-03-02 11:17:01 +0100

    promote myfirst_cookbook_08@0.2.0 to staging_08

---
+++ cookbooks/myfirst_cookbook_08/metadata.rb
@@ -0,0 +1,10 @@
+name 'myfirst_cookbook_08'
+maintainer 'Thierry SARLIN'
+maintainer_email 'ho.tsarlin@cma-cgm.com'
+license 'all_rights'
+description 'Installs/Configures myfirst_cookbook_08'
+long_description 'Installs/Configures myfirst_cookbook_08'
+version '0.2.0'
+
+issues_url 'https://github.com/oswaldderiemaecker/chef-repo-training/myfirst_cookbook_08/issues' if respond_to?(:issues_url)
+source_url 'https://github.com/oswaldderiemaecker/chef-repo-training/myfirst_cookbook_08' if respond_to?(:source_url)
