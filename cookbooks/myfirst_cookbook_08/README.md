c07782400b6d34fcb1b104005002408f67056c8e
ho.tsarlin <tsarlin73@myorganization.com>
2017-03-02 15:19:20 +0100

    Update

---
+++ cookbooks/myfirst_cookbook_08/README.md
@@ -0,0 +1,4 @@
+# myfirst_cookbook
+
+TODO: Enter the cookbook description here.
+
---
+++ cookbooks/myfirst_cookbook_08/recipes/default.rb
@@ -0,0 +1,9 @@
+#
+# Cookbook:: myfirst_cookbook
+# Recipe:: default
+#
+# Copyright:: 2017, The Authors, All Rights Reserved.
+
+package 'vim'
+package 'lynx'
+package 'unzip'
